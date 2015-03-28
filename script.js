c3ec7a42976c64d3d9172c3ff5e54cb3e55559b1
Wendy Leung <wendysarahleung@gmail.com>
2015-03-27 23:00:11 -0700

    Delete responsiveVoice js and jQuery -Add speechSynthesis

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 (function(){
   console.log('activate');
-  var node = document.createElement('script');
-  node.src = 'http://responsivevoice.org/responsivevoice/responsivevoice.js';
-  var jq = document.createElement('script');
-  jq.src = "https://code.jquery.com/jquery-2.1.1.min.js";
-  document.head.appendChild(jq);
-  document.head.appendChild(node);
+  var msg = new SpeechSynthesisUtterance();
   document.body.onmouseup = function(){
     getSelText();
+    window.speechSynthesis.speak(msg);
   }
   function getSelText(){
     var txt = '';
@@ -20,8 +16,7 @@
     } else {
       return;
     }
-    // document.body.innerHTML= txt;
-    responsiveVoice.speak(txt,'US English Female');
+    msg.text = txt;
   }
 
-}());
+}());   
