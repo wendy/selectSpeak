25abd28c57230ffcfdf01d9862bbac768c022ff0
Wendy Leung <wendysarahleung@gmail.com>
2015-03-27 22:38:17 -0700

    Add jQuery

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 (function(){
+  console.log('activate');
   var node = document.createElement('script');
   node.src = 'http://responsivevoice.org/responsivevoice/responsivevoice.js';
+  var jq = document.createElement('script');
+  jq.src = "https://code.jquery.com/jquery-2.1.1.min.js";
+  document.head.appendChild(jq);
   document.head.appendChild(node);
   document.body.onmouseup = function(){
     getSelText();
